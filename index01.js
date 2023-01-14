35427b6a2e14a1979aa18a477e569450f4f71912
caio19071990 <caiosilvacc0@gmail.com>
2023-01-14 20:47:07 -0300

    arquivo modificado

--- index01.js
+++ index01.js
@@ -1,1 +1,10 @@
-console.log('Boa Noite')
+var nome = 'caio';
+var sobrenome = 'silva';
+var idade = 32;
+var cidade = 'Belém';
+
+console.log ('Curriculo');
+console.log('Nome do usuario:' + " " + nome);
+console.log('Sobrenome do usuario é:' + " " + sobrenome);
+console.log('A idade do usuario é :' + " " + idade);
+console.log('Cidade aonde mora:' + " " + cidade);
