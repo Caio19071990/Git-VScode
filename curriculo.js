89127548f4289e3274718f1734e905aa900d4306
Caio Silva <122320638+Caio19071990@users.noreply.github.com>
2023-01-16 19:58:05 -0300

    Update curriculo.js

--- curriculo.js
+++ curriculo.js
@@ -3,7 +3,7 @@ var sobrenome = 'silva';
 var idade = 32;
 var cidade = 'Belém';
 
-console.log ('Curriculo');
+console.log ('CURRICULO PROFISSIONAL');
 console.log('Nome do usuario:' + " " + nome);
 console.log('Sobrenome do usuario é:' + " " + sobrenome);
 console.log('A idade do usuario é :' + " " + idade);
